081aa38377e6b34f96de335b08230c3113032f0b
Julio Realpe <jreapupito@gmail.com>
2017-01-23 02:04:20 -0500

    [ADD] static route to bower components

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
 app.get('/', function (req, res) {
   //res.send('Hello World!');
   res.sendFile(__dirname + '/index.html');
